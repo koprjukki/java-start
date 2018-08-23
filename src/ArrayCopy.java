6923e909e2b5f87277dd74ca77d7070078bb3a81
Illya Kobyliatskiy <flazzygardner1234@gmail.com>
2018-08-24 02:23:26 +0300

    unused

---
+++ src/ArrayCopy.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayCopy {
+    public static void main(String args[]){
+        Scanner scr = new Scanner(System.in);
+        int[] ar = new int[4];
+        System.out.println("Fill Array");
+        for (int i = 0; i < ar.length; i++){
+            ar[i] = scr.nextInt();
+        }
+        int ar1[], ar2[];
+        ar1 = Arrays.copyOfRange(ar,0,2);
+        ar2 = Arrays.copyOfRange(ar,2,4);
+
+        String ar1s = Arrays.toString(ar1);
+        String ar2s = Arrays.toString(ar2);
+        System.out.println(ar1s);
+        System.out.println(ar2s);
+
+
+
+    }
+}
