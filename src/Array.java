9acac02205cfd3e92e2063e54595d969c50cb4f4
Illya Kobyliatskiy <flazzygardner1234@gmail.com>
2018-08-24 01:16:23 +0300

    unused

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String args[]){
+        int ar1[] = {8, -31 , -7, -3, 19, 14, 144, 78, 63, 29};
+        Arrays.sort(ar1, 0 , 10);
+        System.out.println(Arrays.toString(ar1));
+
+
+    }
+}
