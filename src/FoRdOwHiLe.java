2894182b318651cb9f627285e8a47b10bdeaf30b
Illya Kobyliatskiy <flazzygardner1234@gmail.com>
2018-08-24 03:02:01 +0300

    unused

---
+++ src/FoRdOwHiLe.java
@@ -0,0 +1,28 @@
+public class FoRdOwHiLe {
+    public static void main(String args[]){
+
+        for(int ten = 10; ten <= 20; ten++){
+            System.out.println(ten);
+        }
+        System.out.println();
+        int ten = 10;
+        while(ten <= 20){
+            System.out.println(ten);
+            ten++;
+        }
+        System.out.println();
+        int tenten = 10;
+        do {
+            System.out.println(tenten);
+            tenten++;
+        }
+        while(tenten <= 20);
+        System.out.println();
+
+        int[] ar = {10,11,12,13,14,15,16,17,18,19,20};
+        for(int f0reach:ar){
+            System.out.println(f0reach);
+        }
+    }
+
+}
